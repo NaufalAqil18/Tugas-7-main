c021f1a57878d28fcd6e67b2fdc7551ece9d69e7
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2025-04-29 10:46:01 +0700

    membuat view sederhana di main

--- katalog_produk/main/views.py
+++ katalog_produk/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(request) :
+    return HttpResponse("<h1>Selamat Datang di Homepage!</h1><p>Ini adalah halaman utama.</p>")
+
+def produk_list(request):
+    return HttpResponse("<h1>Daftar Produk</h1><p>Ini adalah daftar semua produk.</p>")
+    
+def produk_detail(request, produk_id) :
+    return HttpResponse(f"<h1>Detail Produk {produk_id}</h1><p>Ini detail untuk produk dengan ID {produk_id}.</p>")
+
+def kontak(request) :
+    return HttpResponse("<h1>Kontak Kami</h1><p>Hubungi kami di kelompok4@gmail.com</p>")
